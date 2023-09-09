fb7689cc6e2d70811939da1031224e6a15bb9155
wall-e <wall-e@lotto.com>
2023-09-09 22:48:29 +0545

    feat: added remove owner test case

--- test/multisigwallet.test.js
+++ test/multisigwallet.test.js
@@ -1,25 +1,27 @@
 const MultiSigWallet = artifacts.require("MultisigWallet");
-const { Web3 } = require("web3");
 const { ethers } = require("ethers");
+
 contract("MultisigWallet", (accounts) => {
   let walletInstance;
-  const web3 = new Web3("http://127.0.0.1:7545");
+  const provider = new ethers.JsonRpcProvider("http://127.0.0.1:7545");
+
   beforeEach(async () => {
     walletInstance = await MultiSigWallet.new(
-      [accounts[0], accounts[1]],
+      [accounts[0], accounts[1], accounts[2]],
       2,
       "0x0583aF2aC650f95ce3254242C55Bc7c8ad897F00"
     );
   });
   it("should deposit funds", async () => {
-    const initialBalance = await web3.eth.getBalance(walletInstance.address);
-    const depositAmount = web3.utils.toWei("1", "ether");
-    await web3.eth.sendTransaction({
+    const initialBalance = await provider.getBalance(walletInstance.address);
+    const depositAmount = ethers.parseUnits("1", "ether");
+    const signer = await provider.getSigner(accounts[3]);
+    await signer.sendTransaction({
       from: accounts[3],
       to: walletInstance.address,
       value: depositAmount,
     });
-    const newBalance = await web3.eth.getBalance(walletInstance.address);
+    const newBalance = await provider.getBalance(walletInstance.address);
     assert.equal(
       newBalance - initialBalance,
       depositAmount,
@@ -63,8 +65,42 @@ contract("MultisigWallet", (accounts) => {
     );
     assert.deepEqual(
       await walletInstance.getOwners(),
-      [accounts[0], accounts[1], accounts[5]],
+      [accounts[0], accounts[1], accounts[2], accounts[5]],
       "account 0, 1 and 5 must be owners"
+    );
+    assert.deepEqual(dataFromBC, requiredData, "should be equal");
+  });
+  it("should create remove owner request,confirm the transaction and remove the owner", async () => {
+    let ABI = ["function removeOwner(address arg1)"];
+    await walletInstance.createRemoveOwnerTxn(accounts[0], {
+      from: accounts[0],
+    });
+    const iface = new ethers.Interface(ABI);
+    const signature = iface.encodeFunctionData(ABI[0], [accounts[0]]);
+    const transactionDetails = await walletInstance.getTransaction(0);
+    const dataFromBC = {
+      to: transactionDetails.to,
+      data: transactionDetails.data,
+      executed: transactionDetails.executed,
+      numConfirmations: Number(transactionDetails.numConfirmations),
+    };
+    const requiredData = {
+      to: walletInstance.address,
+      data: signature,
+      executed: false,
+      numConfirmations: 1,
+    };
+    await walletInstance.confirmTransaction(0, { from: accounts[1] });
+    const transactionDetailsg = await walletInstance.getTransaction(0);
+    assert.equal(
+      Number(transactionDetailsg.numConfirmations),
+      2,
+      "should be 2"
+    );
+    assert.deepEqual(
+      await walletInstance.getOwners(),
+      [accounts[2], accounts[1]],
+      "account 1 and 2 must be owners"
     );
     assert.deepEqual(dataFromBC, requiredData, "should be equal");
   });
